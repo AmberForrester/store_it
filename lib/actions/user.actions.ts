bbbe7d71829ad82b871c6fa686b96945907520ca
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-11-10 11:18:36 -0500

    Layout, Nav, Sidebar

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,10 +1,11 @@
 "use server";
 
 import { ID, Query } from "node-appwrite";
-import { createAdminClient } from "../appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
 import { appwriteConfig } from "../appwrite/config";
 import { parseStringify } from "../utils";
 import { cookies } from "next/headers";
+import { avatarPlaceholderUrl } from "@/constants";
 
 
 
@@ -61,7 +62,7 @@ export const createAccount = async ({
             {
                 fullName,
                 email,
-                avatar: "/assets/images/avatar2.png",
+                avatar: avatarPlaceholderUrl,
                 accountId,
             },
         );
@@ -95,4 +96,20 @@ export const verifySecret = async ({
     } catch (error) {
         handleError(error, "Failed to verify OTP.");
     }
+};
+
+export const getCurrentUser = async () => {
+    const { databases, account } = await createSessionClient();
+
+    const result = await account.get();
+
+    const user = await databases.listDocuments(
+        appwriteConfig.databaseId,
+        appwriteConfig.usersCollectionId,
+        [Query.equal("accountId", result.$id)],
+    );
+
+    if(user.total <= 0) return null;
+
+    return parseStringify(user.documents[0]);
 };
